b616752713f605daa9c68a093303f11c51fd0a5b
PPPoE15 <art.viktorov12@gmail.com>
2024-01-04 18:55:33 +0300

    add the annotation and requirements

--- dtos.py
+++ dtos.py
@@ -1,8 +1,10 @@
+from typing import Set
 class DeviceDTO:
-    def __init__(self, protocols, skills, available_from, manufacturer, zigbee_id, model, name, origin_link, description) -> None:
-        self.protocols: set = protocols
-        self.skills: set = skills
-        self.available_from: set = available_from
+    def __init__(self, protocols: Set[str], skills: Set[str], available_from: Set[str], manufacturer: str, 
+                 zigbee_id: str, model: str, name: str, origin_link: str, description: str) -> None:
+        self.protocols: Set[str] = protocols
+        self.skills: Set[str] = skills
+        self.available_from: Set[str] = available_from
         self.manufacturer: str = manufacturer
         self.zigbee_id: str = zigbee_id
         self.model: str = model
--- main.py
+++ main.py
@@ -9,20 +9,24 @@ import dtos
 domen = 'https://zigbee.blakadder.com'
 
 
-def parse_zigbee_blakadder():
-    r = requests.get('https://zigbee.blakadder.com/all.html')
-    soup = bs(r.text)
+def parse_zigbee_blakadder() -> List[dtos.DeviceDTO]:
+    device_count = 0
+    r = requests.get(f'{domen}/all.html')
+    soup = bs(r.text, "html.parser")
     result = soup.find_all('tr')
     devices: List[dtos.DeviceDTO] = []
     for device in result:
         uri = device.find('a').attrs['href']
         url = domen + uri
         devices.append(parse_device_page(url))
+        device_count += 1
+        print(f'Device № {device_count} added')
+    return(devices)
 
 def parse_device_page(origin_link: str) -> dtos.DeviceDTO: 
     r = requests.get(origin_link)
     if str(r.status_code)[0] == '2':
-        soup = bs(r.text)
+        soup = bs(r.text, "html.parser")
         description = str(soup.find_all("span", "small")[0].string)
         name = str(soup.find_all("span")[1].string)
         model = str(soup.find_all("span")[2].string)[6:]
@@ -33,7 +37,11 @@ def parse_device_page(origin_link: str) -> dtos.DeviceDTO:
         manufacturer = 'None'
         sellers_url: set(str) = set()
         for parameters in parameters_table:
-            table_name = str(parameters.find_all("thead")[0].text).strip('\n  ')
+            try:
+                table_name = str(parameters.find_all("thead")[0].text).strip('\n  ')
+            except IndexError:
+                print(f'fail to get table name from {origin_link}')
+                continue
             if table_name == 'Supports:': # check if this table are skills table
                 for skill in parameters.find_all('td'):
                     skills.add(str(skill.string).strip('\n  '))
@@ -56,8 +64,8 @@ def parse_device_page(origin_link: str) -> dtos.DeviceDTO:
         device = dtos.DeviceDTO(protocols, skills, sellers_url, manufacturer, zigbee_id, model, name, origin_link, description)
         return(device)
 
-
     else:
         print(f'Error, response code {r.status_code}\n{url}')
     
-parse_zigbee_blakadder()
+
+print(len(parse_zigbee_blakadder()))
